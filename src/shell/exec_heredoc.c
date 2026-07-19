e87139d3647f2df4e3ab04daa61f85ebcf85ceea
dodo-sudo455 <dodo-sudo455@users.noreply.github.com>
2026-07-19 13:11:35 +0900

    fix: update heredoc handling and improve redirection logic

--- src/shell/exec_heredoc.c
+++ src/shell/exec_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: doyelee <doyelee@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 16:20:12 by doyelee           #+#    #+#             */
-/*   Updated: 2026/07/18 16:08:15 by doyelee          ###   ########.fr       */
+/*   Updated: 2026/07/19 12:49:22 by doyelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,10 @@ static void	_heredoc_readline(t_ctx *c_ref, char *delim)
 }
 
 // unlink로 디렉터리 엔트리 삭제하고 열린 fd로만 접근하면 파일이 디스크에 남지 않아 안전하다고 함.. 알아보기
-static void	_heredoc_input(t_ctx *c_ref, t_redir *red_ref,
-								t_lst *doclst_ref, int heredoc_num)
+static void	_heredoc_input(t_ctx *c_ref, char *fname, char *delim)
 {
-	char	*fname;
-	char	*delim;
 	int		fd;
 
-	delim = red_ref->s;
 	fd = safe_open(c_ref, fname, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	safe_dup2(c_ref, fd, STDOUT_FILENO);
 	safe_close(c_ref, fd);
@@ -61,7 +57,7 @@ static void	_heredoc_input(t_ctx *c_ref, t_redir *red_ref,
 }
 
 static t_error	_heredoc_fork(t_ctx *c_ref, t_redir *red_ref,
-								t_lst *doclst_ref, int heredoc_num)
+								t_lst *doclst_ref, int hd_num)
 {
 	char	*fname;
 	char	*delim;
@@ -70,7 +66,7 @@ static t_error	_heredoc_fork(t_ctx *c_ref, t_redir *red_ref,
 
 	delim = red_ref->s;
 	fname = safe_strjoin(c_ref, "/tmp/minishell-heredoc-",
-		ft_itoa(heredoc_num));
+		ft_itoa(hd_num));
 	red_ref->s = fname;
 	safe_lst_push(c_ref, doclst_ref, fname);
 	pid = safe_fork(c_ref);
@@ -80,49 +76,46 @@ static t_error	_heredoc_fork(t_ctx *c_ref, t_redir *red_ref,
 		return (WEXITSTATUS(status));
 	}
 	else
-		_heredoc_input(c_ref, red_ref, doclst_ref, heredoc_num);
+		_heredoc_input(c_ref, fname, delim);
 }
 
-//static void	_open_redir(t_ctx *c_ref, t_redir *red_ref)
-//{
-//	int	fd;
-
-//	if (red_ref->t == REDIR_IN)
-//		fd = open(red_ref->s, O_RDONLY);
-//	else if (red_ref->t == REDIR_OUT)
-//		fd = open(red_ref->s, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-//	if (fd < 0)
-//		perror("minishe ~~");
-//		exit();
-//}
+static t_error _heredoc_process(t_ctx *c_ref, t_cmd *cmd_ref,
+										t_lst *doclst_ref, int *hd_num)
+{
+	t_lst	*red_node;
+	t_redir	*red_ref;
+	int		status;
+	
+	red_node = cmd_ref->redlst.next;
+	while (red_node != &cmd_ref->redlst)
+	{
+		red_ref = red_node->data;
+		if (red_ref->t == REDIR_HDOC)
+		{
+			status = _heredoc_fork(c_ref, red_ref, doclst_ref, hd_num);
+			if (status == 130)
+				return (ERROR_ABORT);
+			*hd_num += 1;
+		}
+		red_node = red_node->next;
+	}
+	return (ERROR_OK);
+}
 
 t_error	exec_heredoc(t_ctx *c_ref, t_lst *cmdlst_ref, t_lst *doclst_ref)
 {
 	t_lst	*cmd_node;
-	t_lst	*red_node;
 	t_cmd	*cmd_ref;
-	t_redir	*red_ref;
-	int		heredoc_num;
-	int		status;
+	int		hd_num;
 
-	heredoc_num = 0;
+	hd_num = 0;
 	cmd_node = cmdlst_ref->next;
 	while (cmd_node != cmdlst_ref)
 	{
 		cmd_ref = cmd_node->data;
-		red_node = cmd_ref->redlst.next;
-		while (red_node != &cmd_ref->redlst)
-		{
-			red_ref = red_node->data;
-			if (red_ref->t == REDIR_HDOC)
-			{
-				status = _heredoc_fork(c_ref, red_ref, doclst_ref, heredoc_num);
-				if (status == 130)
-					return (ERROR_ABORT);
-				heredoc_num += 1;
-			}
-			red_node = red_node->next;
-		}
+		if (_heredoc_process(c_ref, cmd_ref, doclst_ref, &hd_num) != ERROR_OK)
+			return (ERROR_ABORT);
 		cmd_node = cmd_node->next;
 	}
+	return (ERROR_OK);
 }
