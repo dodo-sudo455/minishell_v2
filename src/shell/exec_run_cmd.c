3933f028d1ef043cfc88a70606b29759d4c76b9b
dodo-sudo455 <dodo-sudo455@users.noreply.github.com>
2026-07-28 16:13:28 +0900

    fix: update error handling in _handle_redir to return error code

--- src/shell/exec_run_cmd.c
+++ src/shell/exec_run_cmd.c
@@ -58,7 +58,10 @@ static int	_handle_redir(t_ctx *c_ref, t_cmd *cmd_ref)
 		red_ref = nod_ref->data;
 		_get_redir_fd(c_ref, red_ref, &fd, &fd2);
 		if (fd < 0)
-			return (seterr(c_ref, ERROR_OPEN, NULL, red_ref->s));
+		{
+			seterr(c_ref, ERROR_OPEN, NULL, red_ref->s);
+			return (1);
+		}
 		safe_dup2(c_ref, fd, fd2);
 		close(fd);
 		nod_ref = nod_ref->next;
