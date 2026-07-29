23c88758ac7059751215c412712fd490de494e7f
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-29 16:59:51 +0900

    fix: exit behavior with arg

--- src/lib/cmd_built_exit.c
+++ src/lib/cmd_built_exit.c
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_built_exit.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: doyelee <doyelee@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 12:39:34 by doyelee           #+#    #+#             */
-/*   Updated: 2026/07/28 14:31:28 by doyelee          ###   ########.fr       */
+/*   Updated: 2026/07/29 16:58:43 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
+#define LL_MAX "9223372036854775807"
+#define LL_MIN "9223372036854775808"
 
-static bool	_exit_is_numeric(const char *s)
+static bool	_is_numeric(const char *s)
 {
 	if (!s || !*s)
 		return (false);
@@ -20,21 +22,84 @@ static bool	_exit_is_numeric(const char *s)
 		s++;
 	if (!*s)
 		return (false);
+	while (*s && *s == ' ')
+		s++;
 	while (*s)
 	{
-		if (*s < '0' || *s > '9')
+		if (*s == ' ')
+			break ;
+		if (!ft_isdigit(*s))
+			return (false);
+		s++;
+	}
+	while (*s)
+	{
+		if (*s != ' ')
 			return (false);
 		s++;
 	}
 	return (true);
 }
 
+static bool	_has_ll_overflow(const char *s)
+{
+	int		sign;
+	int		len;
+
+	sign = 1;
+	while (*s == ' ')
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (*s == '0')
+		s++;
+	len = 0;
+	while (ft_isdigit(s[len]))
+		len++;
+	if (len > 19)
+		return (true);
+	if (len < 19)
+		return (false);
+	if (sign == -1)
+		return (ft_strncmp(s, LL_MIN, 19) > 0);
+	return (ft_strncmp(s, LL_MAX, 19) > 0);
+}
+
+static int	_exit_atoi(const char *s)
+{
+	unsigned long long	val;
+	int					sign;
+
+	val = 0;
+	sign = 1;
+	while (*s == ' ')
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign = -1;
+		s++;
+	}
+	while (ft_isdigit(*s))
+	{
+		val = val * 10 + (*s - '0');
+		s++;
+	}
+	if (sign == -1)
+		return ((int)(-(long long)val));
+	return ((int)val);
+}
+
 static void	_exit_numeric_error(t_ctx *c_ref, const char *arg)
 {
 	ft_putendl_fd("exit", STDOUT_FILENO);
-	write(2, "minishell: exit: ", 17);
-	write(2, arg, ft_strlen(arg));
-	write(2, ": numeric argument required\n", 28);
+	util_puterr("minishell: exit: ");
+	util_puterr(arg);
+	util_puterr(": numeric argument required\n");
 	ctx_clear(c_ref);
 	exit(2);
 }
@@ -55,26 +120,27 @@ int	cmd_built_exit(t_ctx *c_ref, const t_cmd *cmd_ref)
 {
 	t_lst	*arg_node;
 	char	*arg;
-	int		exit_status;
+	int		ret;
 
 	if (ft_lst_size(&cmd_ref->arglst) == 1)
 	{
-		ctx_clear(c_ref);
-		ft_putendl_fd("exit", STDOUT_FILENO);
-		exit(0);
+		ret = c_ref->status;
 	}
-	arg_node = cmd_ref->arglst.next->next;
-	arg = arg_node->data;
-	if (!_exit_is_numeric(arg))
-		_exit_numeric_error(c_ref, arg);
-	if (arg_node->next != &cmd_ref->arglst)
+	else
 	{
-		ft_putendl_fd("exit", STDOUT_FILENO);
-		write(2, "minishell: exit: too many arguments\n", 36);
-		return (1);
+		arg_node = cmd_ref->arglst.next->next;
+		arg = arg_node->data;
+		if (!_is_numeric(arg) || _has_ll_overflow(arg))
+			_exit_numeric_error(c_ref, arg);
+		if (arg_node->next != &cmd_ref->arglst)
+		{
+			ft_putendl_fd("exit", STDOUT_FILENO);
+			util_puterr("minishell: exit: too many arguments\n");
+			return (1);
+		}
+		ret = _exit_atoi(arg);
 	}
-	exit_status = ft_atoi(arg);
 	ctx_clear(c_ref);
 	ft_putendl_fd("exit", STDOUT_FILENO);
-	exit(exit_status);
+	exit((unsigned char)ret);
 }
