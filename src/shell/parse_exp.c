2a13e66eaf9d455f7fb9fedb736d89b87facd331
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-29 14:01:24 +0900

    fix: should not skip first token

--- src/shell/parse_exp.c
+++ src/shell/parse_exp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_exp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: doyelee <doyelee@student.42gyeongsan.kr    +#+  +:+       +#+        */
+/*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 17:10:02 by minseobk          #+#    #+#             */
-/*   Updated: 2026/07/19 14:03:59 by doyelee          ###   ########.fr       */
+/*   Updated: 2026/07/29 13:57:58 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ void	parse_expand(t_ctx *c_ref, t_lst *toklst_ref)
 	{
 		tok_ref = (t_token *)nod_ref->data;
 		if (token_is_word(tok_ref)
-			&& (nod_ref->prev != toklst_ref
-				&& ((t_token *)nod_ref->prev->data)->t != TOKEN_REDIR_HDOC))
+			&& (nod_ref->prev == toklst_ref
+				|| ((t_token *)nod_ref->prev->data)->t != TOKEN_REDIR_HDOC))
 			ctx_expand(c_ref, &tok_ref->s);
 		nod_ref = nod_ref->next;
 	}
