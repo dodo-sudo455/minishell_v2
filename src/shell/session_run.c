4a40355cdfb16840473c1bd7b90d513ea344b3b9
msisdev <33077839+msisdev@users.noreply.github.com>
2026-07-30 13:48:09 +0900

    style: norm err

--- src/shell/session_run.c
+++ src/shell/session_run.c
@@ -6,7 +6,7 @@
 /*   By: minseobk <minseobk@student.42gyeongsan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 15:01:06 by minseobk          #+#    #+#             */
-/*   Updated: 2026/07/29 19:22:07 by minseobk         ###   ########.fr       */
+/*   Updated: 2026/07/30 13:47:38 by minseobk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 void	session_run(t_ctx *c_ref, t_session *s_ref)
 {
 	if (session_prompt(c_ref, s_ref) != 0)
-	return ;
+		return ;
 	if (session_parse(c_ref, s_ref) != 0)
-	return ;
+		return ;
 	if (session_exec(c_ref, s_ref) != 0)
-	return ;
+		return ;
 }
 
 int	session_prompt(t_ctx *c_ref, t_session *s_ref)
@@ -30,26 +30,24 @@ int	session_prompt(t_ctx *c_ref, t_session *s_ref)
 
 int	session_parse(t_ctx *c_ref, t_session *s_ref)
 {
-int	status;
+	int	status;
 
 	if (!parse_is_quote_ok(s_ref->input))
-	return (ctx_abort(c_ref, ERROR_QUOTE, NULL, NULL));
+		return (ctx_abort(c_ref, ERROR_QUOTE, NULL, NULL));
 	parse_tokenize(c_ref, s_ref->input, &s_ref->toklst);
 	parse_expand(c_ref, &s_ref->toklst);
 	parse_split_word(c_ref, &s_ref->toklst);
 	parse_quote(c_ref, &s_ref->toklst);
-	if ((status = parse_command(c_ref, &s_ref->toklst, &s_ref->cmdlst)) != 0)
-	return (status);
-	return (0);
+	return (parse_command(c_ref, &s_ref->toklst, &s_ref->cmdlst));
 }
 
 int	session_exec(t_ctx *c_ref, t_session *s_ref)
 {
-int	status;
+	int	status;
 
 	status = exec_hdoc(c_ref, &s_ref->cmdlst);
 	if (status != 0)
-	return (status);
+		return (status);
 	exec_run(c_ref, &s_ref->cmdlst);
 	return (0);
 }
